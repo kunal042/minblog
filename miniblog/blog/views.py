85a44a14b90572f37af1110450a5be016c933cce
kunal kumar <k1509kunal@gmail.com>
2024-05-08 09:33:23 +0530

    all page and nav link set

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+# Home Page
+def home(request):
+    
+    return render(request, 'blog/home.html' , )
+
+
+# About Page
+def about(request):
+    
+    return render(request, 'blog/about.html' , )
+
+
+# About Page
+def contact(request):
+    
+    return render(request, 'blog/contact.html' , )
+
+
+# Dashboard
+def dashboard(request):
+    
+    return render(request, 'blog/dashboard.html' , )
+
+
+# logout
+def user_logout(request):
+    
+    return HttpResponseRedirect("/")
+
+
+# Signup
+def user_signup(request):
+    form = UserCreationForm()
+    
+    return render(request, 'blog/signup.html' , {"form":form })
+
+# login
+def user_login(request):
+    
+    return render(request, 'blog/login.html' , )
+
+
--- miniblog/miniblog/urls.py
+++ miniblog/miniblog/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("logout/", views.user_logout, name="logout"),
+    path("login/", views.user_login, name="login"),
+    path("signup/", views.user_signup, name="signup"),
 ]
