6113ccef14351a773739a0d2e6bfe5bf3cfb5233
kunal kumar <k1509kunal@gmail.com>
2024-05-08 16:14:36 +0530

    singup, login logout

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, LoginUpForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
 
 
 # Home Page
@@ -28,19 +30,47 @@ def dashboard(request):
 
 # logout
 def user_logout(request):
+    logout(request)
     
     return HttpResponseRedirect("/")
 
 
 # Signup
 def user_signup(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            un = form.cleaned_data['username']
+            fn = form.cleaned_data['first_name']
+            ln = form.cleaned_data['last_name']
+            em = form.cleaned_data['email']
+            
+
+            messages.success(request, "Congartulations!! Yor become an Author.")
+            
+            form.save()
+        form = SignUpForm()
+    else:
+        form = SignUpForm()
     
     return render(request, 'blog/signup.html' , {"form":form })
 
 # login
 def user_login(request):
-    
-    return render(request, 'blog/login.html' , )
-
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginUpForm(request=request, data=request.POST)
+            if form.is_valid():
+                un = form.cleaned_data['username']
+                upass = form.cleaned_data["password"]
+                user = authenticate(username=un, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in Successfully')
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form = LoginUpForm()
+        return render(request, 'blog/login.html' ,  {'form': form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
 
